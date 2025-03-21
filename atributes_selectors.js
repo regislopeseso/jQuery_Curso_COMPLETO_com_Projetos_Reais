7da7e0e2b2d8ec0af8da588d640234c91df49dac
Regis <regis.lopes.eso@gmail.com>
2025-03-21 17:18:16 -0300

    Add class 16. Seletores - Filtros Básicos

---
+++ atributes_selectors.js
@@ -0,0 +1,17 @@
+// Verificando se possui um atributo ->
+// $('.header-border li[href]').text('Link')
+
+// Verificando se possui mais de um atributo ->
+// $('.header-border li [href][title]').text('Link')
+
+// Verificando um valor para o atributo ->
+// $('.header-border li [href="about.html"]').text()
+
+// Verificando os atributos que COMEÇAM com uma letra específica
+// $('.header-border li [href^="a"]').text()
+
+// Verificando os atributos que TERMINAM a expressão informada ->
+// $('.header-border li [href$="html"]').text()
+
+// Verificando os atributos que CONTÉM a expressão informada ->
+// $('.header-border li [href*="in"]').text()

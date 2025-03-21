1c5a297e7620be11224a94e8455d2aec295b4c97
Regis <regis.lopes.eso@gmail.com>
2025-03-21 17:45:53 -0300

    Add class 17. Seletores - Filtros do CSS3

---
+++ basicFilters_selectors.js
@@ -0,0 +1,23 @@
+// Selecionando o primeiro elemento de uma lista
+// $(".cars_list li:first").click(function () {
+//        alert("ok");
+//      });
+
+// Selecionando o último elemento de uma lista
+// $(".cars_list li:last").click(function () {
+//   alert("ok");
+// });
+
+// Selecionando o enésimo elemento de uma lista
+// $(".cars_list li:eq(2 ou 3 ou 4 ... enésimo)").click(function () {
+//   alert("ok");
+// });
+
+// Selecionando os itens pares elemento de uma lista (as listas começam com 0)
+// $(".cars_list li:even").css('background-color','#ccc')
+
+// Selecionando os itens ímpares elemento de uma lista
+// $(".cars_list li:odd").css('background-color','#ccc')
+
+// Selecionando os itens de uma lista a partir de um index informado
+// $('.cars_list li:gt(1)').css('opacity', '0.3')

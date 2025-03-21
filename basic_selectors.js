63da9a38a5ca09b0ce5c64c73399fcbdcebd62c2
Regis <regis.lopes.eso@gmail.com>
2025-03-21 15:56:53 -0300

    Add class 14. Seletores - Usando Hierarquia

---
+++ basic_selectors.js
@@ -0,0 +1,14 @@
+//! Tag name -> $('h1').css('color', 'red')
+
+//* Id -> $('#logo').html('Negócios')
+
+//? Class ->
+//$(".paragrafo").animate(
+//  {
+//      fontSize: "50px",
+//      opacity: 0.7,
+//  }, 5000);
+
+//! Seletor composto -> $('header nav.menu').css('border','solid 1px orange')
+
+//* Multiple Selectors -> $('form, section').css('background-color','#ccc')
---
+++ heritage_selectors.js
@@ -0,0 +1,19 @@
+// Elementos descendentes ->
+// $('main h1').css('color', 'red')
+
+// Elemento descendente direto ->
+// $('main>.title').css('border', 'solid 2px red')
+
+// $('main>div>h1').css('border', 'solid 2px red')
+
+// Elemento (apenas 1) irmão mais próximo -> +
+// $(".paragrafo+p").css({
+//     "background-color": "red",
+//     color: "white",
+//   });
+
+// TODOS elementos irmão mais próximos -> ~
+// $(".paragrafo~p").css({
+//     "background-color": "red",
+//     color: "white",
+//   });

8f473164f14a1739f381709dca6f43dde4db00a7
Regis <regis.lopes.eso@gmail.com>
2025-03-24 13:58:26 -0300

    Add class 27. Manipulando propriedades

---
+++ secction 4 - DOM manipulation/Manipulation/attributes_manipulation.js
@@ -0,0 +1,6 @@
+//! Manipulation: attributes
+// Retornando ou definindo um atributo
+// ex.1: $('img').attr('src')
+// ex.2: $('img').attr('src','assets/images/jquery.jpg')
+// Removendo um atributo
+// ex.1: $('img').attr('src','assets/images/jquery.jpg')
---
+++ secction 4 - DOM manipulation/Manipulation/properties_manipulation.js
@@ -0,0 +1,11 @@
+//! Manipulation: Properties
+// Retorna bool ou define bool de uma propriedade
+// $("form [type=checkbox]").prop("checked");
+// $("form [type=checkbox]").prop("checked", true);
+// $("form [type=checkbox]").prop("checked", false);
+// Adiciona uma propriedade:
+// $('h1').prop('nome', 'Titulo')
+// Lê o valor de uma propriedade
+// $('h1').prop('nome')
+// Remove uma propriedade que tenha sido criada (não nativa)
+//$('h1').removeProp('nome')

c6321799b6b0b18cd7f5ab299e13ee74e883c791
Regis <regis.lopes.eso@gmail.com>
2025-03-24 14:51:32 -0300

    Add class 30. Mudando dimensões de um elemento

---
+++ secction 4 - DOM manipulation/Manipulation/changingElementDimensions_manipulation.js
@@ -0,0 +1,14 @@
+//! Manipulation: changing the dimensions of an element
+// Retorna ou define a lagura de um elemento:
+// ex.1: $('.paragrafo').width()
+// ex.2: $('.paragrafo').width(930)
+
+// Retorna ou define a largura de um elemento levando em conta o padding
+// ex.1: $('.paragrafo').innerWidth()
+
+// Retorna ou define a altura de um elemento:
+// ex.1: $('.paragrafo').height()
+// ex.2: $('.paragrafo').height(930)
+
+// Retorna ou define a largura de um elemento levando em conta o padding
+// ex.1: $('.paragrafo').innenHeight()

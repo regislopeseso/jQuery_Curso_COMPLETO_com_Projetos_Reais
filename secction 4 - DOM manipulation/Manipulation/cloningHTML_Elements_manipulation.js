881c0dcdfdc3586daf6fd118874806899ba01246
Regis <regis.lopes.eso@gmail.com>
2025-03-24 17:53:51 -0300

    Add class 35. Clonando elementos HTML

---
+++ secction 4 - DOM manipulation/Manipulation/cloningHTML_Elements_manipulation.js
@@ -0,0 +1,29 @@
+//! Manipulation: Cloning Html elements
+// $(document).ready(function () {
+//   let el = $(".title").clone();
+
+//   el.appendTo("section main");
+// });
+
+// By default events are not clonned,
+//  if wantend the parameter "true" must be passed to .clone() => .clone(true)
+// $(document).ready(function () {
+//   $(".title").click(function () {
+//     $(this).css("outline", "5px solid green");
+//   });
+
+//   let el = $(".title").clone(true);
+
+//   el.appendTo("section main");
+// });
+
+// This clones also the event
+// $(document).ready(function () {
+//   $(".title").click(function () {
+//     $(this).css("outline", "5px solid green");
+//   });
+
+//   let el = $(".title").clone(true);
+
+//   el.appendTo("section main");
+// });

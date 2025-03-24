b72292c0e03344d5ecbf6a1d10627366df8c0cb6
Regis <regis.lopes.eso@gmail.com>
2025-03-24 11:02:06 -0300

    Add class 23. Seletores - Criando nossos próprios filtros

---
+++ selectors/selfMadeFilters_selectors.js
@@ -0,0 +1,10 @@
+// Selectors: self created filters
+// $.expr[":"].cars_21thCentury = function (el) {
+//   return $(el).data("ano") > 2000;
+// };
+
+// $.expr[":"].carsFilteredByYear = $.expr.createPseudo(function (year) {
+//   return function (el) {
+//     return $(el).data("ano") <= year;
+//   };
+// });

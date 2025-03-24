77fa2bb9460866a3ce2964ef162d383ee3943661
Regis <regis.lopes.eso@gmail.com>
2025-03-24 08:57:23 -0300

    Add class 20. Botão para exibir & ocultar os carros

--- visibilityFilters_selectos.js
+++ visibilityFilters_selectos.js
@@ -6,12 +6,24 @@
 // $(".cars_list li:visible").hide();
 
 //!? Letting items be either hidden or shown with two separate functions
+//* <button type="button" id="show_cars">Mostrar carros ocultos</button>
+//* <button type="button" id="hide_cars">Ocultar carros</button>
+
 // $("#show_cars").click(function () {
 //   $(".cars_list li:hidden").css("text-decoration", "underline").show();
 // });
 
-// $("#toggle_cars").click(function () {
+// $("#hide_cars").click(function () {
 //   $(".cars_list li:visible").hide();
 // });
 
 //!? Letting items be either hidden or shown with only one function
+//* <button type="button" id="toggle_cars">Exibir/Ocultar os carros</button>
+
+// $("#toggle_cars").click(function () {
+//   let hiddenCars = $(".cars_list li:hidden").length;
+
+//   let method = hiddenCars > 0 ? "show" : "hide";
+
+//   eval(`$('.cars_list li').${method}()`);
+// });

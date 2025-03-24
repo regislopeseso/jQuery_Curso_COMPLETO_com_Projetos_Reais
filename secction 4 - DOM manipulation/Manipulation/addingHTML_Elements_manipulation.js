d8534ce83d7358651689bd34a2314b4dd3e12fc4
Regis <regis.lopes.eso@gmail.com>
2025-03-24 15:28:11 -0300

    Add class 32. Adicionando elementos HTML Parte 1

---
+++ secction 4 - DOM manipulation/Manipulation/addingHTML_Elements_manipulation.js
@@ -0,0 +1,14 @@
+//! Manipulation: adding HTML elements
+
+// Adicionando um elemento ao FINAL
+// $(".menu ul").append('<li><a href="#">News</a></li>');
+
+// Adicionando um elemento ao FINAl de outro por meio de uma sintaxe difente
+// $('<li><a href="#">News 2</a>').appendTo(".menu ul");
+
+// Adicionando um elemento no INÍCIO:
+// $('.menu ul').prepend('<li><a href="#">Products</a></li>');
+// $('<li><a href="#">News 2</a></li>').prependTo(".menu ul");
+
+// Adicionando um elemento no INÍCIO de outro por meio de uma sintaxe difente
+// $('<li><a href="#">News 2</a></li>').prependTo(".menu ul");

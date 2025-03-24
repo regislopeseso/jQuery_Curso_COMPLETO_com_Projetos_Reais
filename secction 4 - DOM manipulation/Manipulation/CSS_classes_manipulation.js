2791d1e822022c3f5cde53d4165306d46048b29e
Regis <regis.lopes.eso@gmail.com>
2025-03-24 13:24:03 -0300

    Add class 25. Manipulando classes CSS

---
+++ secction 4 - DOM manipulation/Manipulation/CSS_classes_manipulation.js
@@ -0,0 +1,18 @@
+//! Manipulation: CSS classes
+// Adicionando uma classe:
+// ex.1: $("header .menu ul li:last").addClass("stylized_menu")
+// ex.2: $("header .menu ul li:last a").addClass("stylized_menu")
+
+// Removendo uma classe.:
+// $("header .menu ul li:last a").removeClass("stylized_menu")
+
+// Alternando uma classe:
+// $(this).toggleClass("stylized_menu");
+// ex.:
+// $("header .menu ul li a").click(function (event) {
+//   event.preventDefault();
+//   $(this).toggleClass("stylized_menu");
+// });
+
+// Verificando se um elemento possui uma classe:
+// $('header .menu ul li:last a').hasClass('stylized_menu')

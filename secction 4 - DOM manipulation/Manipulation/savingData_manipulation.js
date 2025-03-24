1fb3e044548c9f9c33795b6b6384bac780f55b8c
Regis <regis.lopes.eso@gmail.com>
2025-03-24 14:30:23 -0300

    Add class 29. Armazenando dados nos elementos

---
+++ secction 4 - DOM manipulation/Manipulation/savingData_manipulation.js
@@ -0,0 +1,10 @@
+//! Manipulation: Storing and Manipulating Data on Elements
+// Retornando ou definindo o dado de um elemento
+// $('.cars_list li:fisrt').data('ano','2030')
+// $(".cars_list li").click(function () {
+//   console.log(`Esse carro é do ano de ${$(this).data("ano")}`);
+// });
+//
+// $('.cars_list li:first').data('anoCompra', '2010')
+// $('.cars_list li:first').removeData('anoCompra')
+// $('.cars_list li:first').data('anoCompra') => returns undefined

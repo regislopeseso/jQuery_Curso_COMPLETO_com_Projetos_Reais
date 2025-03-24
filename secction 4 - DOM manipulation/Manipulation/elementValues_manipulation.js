607901cc71a6a9884032bec701c529a5d71472f0
Regis <regis.lopes.eso@gmail.com>
2025-03-24 14:18:43 -0300

    Add class 28. Manipulando valores dos elementos

---
+++ secction 4 - DOM manipulation/Manipulation/elementValues_manipulation.js
@@ -0,0 +1,5 @@
+//! Manipulation: element values
+// Retornando ou definindo o valor de um campo:
+// $('form [type=text]:first').val()
+// $('form [type=text]').val('teste')
+// $('form [type=date]').val('1992-05-18')

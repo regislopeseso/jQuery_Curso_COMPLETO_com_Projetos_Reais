a0043005784899f3f9579e646de22552d54f6ed7
Regis <regis.lopes.eso@gmail.com>
2025-03-24 08:44:12 -0300

    Add class 18. Seletores - filtro de visibilidade

---
+++ contentFilters_selectors.js
@@ -0,0 +1,24 @@
+//! Selectors: content filter
+// Seleciona o primeiro item olhando para todas as listas que possuem a classe informada
+// $(".cars_list li:first").css("text-decoration", "underline");
+
+// Seleciona o primeiro item de CADA lista que possua a classe informada
+// $(".cars_list li:first-child").css("text-decoration", "underline");
+
+// Seleciona o último item olhando para todas as listas que possuem a classe informada
+// $(".cars_list li:last").css("text-decoration", "underline");
+
+// Seleciona o último item de CADA lista que possua a classe informada
+// $(".cars_list li:last-child").css("text-decoration", "underline");
+
+// Seleciona os itens pares de uma lista, nesse caso a contagem começa com 1
+// $(".cars_list li:nth-child(even)").css("background-color", "#ccc");
+
+// Seleciona os itens ímpares de uma lista, nesse caso a contagem começa com 1
+// $(".cars_list li:nth-child(odd)").css("background-color", "#ccc");
+
+// Seleciona os itens CADA lista (que tenham a classe informada) que possuem o index informado, nesse caso a contagem começa com 1
+// $(".cars_list li:nth-child(2 ou 3 ou 56 ou ..)").css("background-color", "#ccc");
+
+// Seleciona os itens de CADA lista (que tenham a classe informada) cujo o index seja a contagem que começa com o útimo item até o valor informado
+// $(".cars_list li:nth-last-child(3").css("font-weight", "bold");
---
+++ visibilityFilters_selectos.js
@@ -0,0 +1,17 @@
+//! Selectors: visibility filter
+// Seleciona os elementos que estão ocultos;
+// $('.cars_list li:hidden').show()
+
+// Seleciona os elementos que estão visíveis;
+// $(".cars_list li:visible").hide();
+
+//!? Letting items be either hidden or shown with two separate functions
+// $("#show_cars").click(function () {
+//   $(".cars_list li:hidden").css("text-decoration", "underline").show();
+// });
+
+// $("#toggle_cars").click(function () {
+//   $(".cars_list li:visible").hide();
+// });
+
+//!? Letting items be either hidden or shown with only one function

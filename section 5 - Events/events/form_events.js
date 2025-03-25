6ca6beea1b0eda23c73777643dd5599a21539d19
Regis <regis.lopes.eso@gmail.com>
2025-03-25 10:03:42 -0300

    Add class 41. Eventos de formulário parte 1

---
+++ section 5 - Events/events/form_events.js
@@ -0,0 +1,22 @@
+//! Events: form events
+// Evento ocorre quando um elemento é colocado em foco: .focus()
+// $("[name]").focus(function () {
+//   $(this).css("background", "yellow");
+// });´
+
+// Evento ocorre quando um elemento é colocado em foco: .focus()
+// Similar ao .focus(), porém engloba "child elements"
+// $(".form-group").focusin(function () {
+//   $(this).css("background", "yellow");
+// });
+
+// Evento ocorre quando um elemento deixa de estar em foco: .blur()
+// $("[name]").blur(function () {
+//   alert(`Você digitou ${$(this).val()}`);
+// });
+
+// Evento ocorre quando um elemento deixa de estar em foco: .focusout()
+// Similar ao .blur(), porém engloba "child elements"
+// $(".form-group").focusout(function () {
+//   alert(`Você digitou ${$(this).val()}`);
+// });

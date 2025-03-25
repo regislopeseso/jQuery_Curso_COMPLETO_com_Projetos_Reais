41220a74ea26d2f4e0a58a1628419e683b970322
Regis <regis.lopes.eso@gmail.com>
2025-03-25 10:52:30 -0300

    Add class 42. Eventos de formulário parte 2

--- section 5 - Events/events/form_events.js
+++ section 5 - Events/events/form_events.js
@@ -20,3 +20,28 @@
 // $(".form-group").focusout(function () {
 //   alert(`Você digitou ${$(this).val()}`);
 // });
+
+// Evento ocorre quando um campo é selecionado
+// e tem seu valor alterado (ex.: lista, radio, etc): .change()
+// $("[name]").change(function () {
+//   alert(`Valor do campo ${$(this).val()}`);
+// });
+
+// Evento ocorre quando um valor (por ex. texto) é selecionado dentro de um campo: .select()
+// $("[name]").select(function () {
+//   alert(`Valor do campo ${$(this).val()}`);
+// });
+
+// Evento ocorre ao submeter um formulário: .submit()
+// $("form").submit(function (event) {
+//   event.preventDefault();
+
+//   console.log($(this).serializeArray());
+// });
+
+// Ex. de .find()
+// $("#link_logo").click(function (event) {
+// event.preventDefault();
+
+// $(this).find("img").attr("src", "img/screenshot_1.png");
+// });

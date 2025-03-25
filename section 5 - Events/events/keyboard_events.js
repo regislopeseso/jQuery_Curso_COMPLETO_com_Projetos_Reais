481ef20b241149203cd8b904ca3168e3758c5e04
Regis <regis.lopes.eso@gmail.com>
2025-03-25 09:44:11 -0300

    Add class 40. Eventos de Teclado

---
+++ section 5 - Events/events/keyboard_events.js
@@ -0,0 +1,18 @@
+//! Events: keyboard events
+// Evento occorre quando uma tecla qualquer do teclado é pressionada: .keydown()
+// $("#form_login [name = username]").keydown(function (event) {
+//   alert(`você apertou a tecla ${event.keyCode}`);
+// });
+
+// Evento ocorre durante o tempo em que uma tecla é mantida pressionada: .keypress()
+// Este evento é similar ao .keydown() porém não detecta algumas teclas tais como o shift, esc, del
+// Sua vantagem sobre o keydown é que ele detecta se uma letra pressionada está em maiúscula ou minúscula.
+// $("#form_login [name = username]").keypress(function (event) {
+//   alert(`você apertou a tecla ${event.keyCode}`);
+// });
+
+// Evento occorre quando uma tecla qualquer do teclado é liberada: .keyup()
+// faz referência apenas à última tecla utilizada.
+// $("#form_login [name = username]").keyup(function (event) {
+//   alert(`você apertou a tecla ${event.keyCode}`);
+// });

361aa017f0805673bd605a05638bf88ed70be219
Regis <regis.lopes.eso@gmail.com>
2025-03-26 11:02:48 -0300

    Add class 62. Animações personalizadas

---
+++ section 6 - Animations/animations/custom_animations.js
@@ -0,0 +1,35 @@
+//! Animations: customizing: .animate()
+// Apenas valores numéricos são aceitos
+// Atenção! Cores não podem ser alteradas
+
+// Exemplos:
+// $("h1").animate({
+//   fontSize: "50px",
+// });
+
+// $(".course_item h4").on("click", function () {
+//   $(this).animate(
+//     {
+//       wordSpacing: "+=5px",
+//     },
+//     200
+//   );
+// });
+
+// $(".course_item").on("click", function () {
+//   $(this).animate(
+//     {
+//       padding: "+=20px",
+//     },
+//     1500
+//   );
+// });
+
+// $("footer p").on("click", function () {
+//   $(this).animate(
+//     {
+//       letterSpacing: "+=10px",
+//     },
+//     2000
+//   );
+// });

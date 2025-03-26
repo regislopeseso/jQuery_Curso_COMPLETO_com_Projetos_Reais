06c6f26b24051f982536ccf9ca34ca0eb4038739
Regis <regis.lopes.eso@gmail.com>
2025-03-26 14:50:06 -0300

    Add class 67. Cadastrando livros

--- section 7 - Node server/server.js
+++ section 7 - Node server/server.js
@@ -1,11 +1,30 @@
 const express = require("express");
-
+const nedb = require("nedb");
+const bodyParser = require("body-parser");
+const db = new nedb({
+  filename: "./database.db",
+  autoload: true,
+});
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/books", (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.post("/books", (req, res) => {
+  db.insert(
+    {
+      name: req.body.name,
+      author: req.body.author,
+    },
+    (err, doc) => {
+      res.json(doc);
+    }
+  );
 });
 
 app.listen(3000, () => {

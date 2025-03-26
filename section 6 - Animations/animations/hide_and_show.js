b11a856d81d81da065f473829b29b6ede0e13728
Regis <regis.lopes.eso@gmail.com>
2025-03-26 09:37:49 -0300

    Add class 58. Animações básicas

---
+++ section 6 - Animations/animations/hide_and_show.js
@@ -0,0 +1,8 @@
+//! Escondendo um elemento:
+// $(".courses_list div:first").hide(2000);
+
+// Exibindo um elemento:
+// $(".courses_list div:hidden").show(1000);
+
+// Exibe e esconde um elemento alternadamente:
+// $(".courses_list .course_item:eq(2)").toggle("slow");

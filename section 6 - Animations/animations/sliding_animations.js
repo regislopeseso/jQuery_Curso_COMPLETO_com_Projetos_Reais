166e66ced44a4162a54cfad87963630378cd7c9e
Regis <regis.lopes.eso@gmail.com>
2025-03-26 10:33:58 -0300

    Add class 60. Animações "Sliding"

---
+++ section 6 - Animations/animations/sliding_animations.js
@@ -0,0 +1,17 @@
+//! Animations: sliding
+
+//? .slideUp()
+// $(".courses_list .course_item_inner:eq(4)").slideUp("fast");
+// $(".courses_list .course_item_inner:eq(4)").slideUp(1000);
+
+//? .slideDown()
+// $(".courses_list .course_item_inner:eq(4)").slideDown("fast");
+
+//? .slideToggle()
+// $(".btn_toggle_item").on("click", function () {
+//   $(this)
+//     .prev(".course_item_inner")
+//     .slideToggle(1000, function () {
+//       console.log("Sumi ou apareci");
+//     });
+// });

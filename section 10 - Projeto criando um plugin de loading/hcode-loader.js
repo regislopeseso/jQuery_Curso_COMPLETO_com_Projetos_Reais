2c724849d27864a0360420efd55162dca8ea4c75
Regis <regis.lopes.eso@gmail.com>
2025-03-27 10:43:55 -0300

    Add class 82. Função para "unload"

--- section 10 - Projeto criando um plugin de loading/hcode-loader.js
+++ section 10 - Projeto criando um plugin de loading/hcode-loader.js
@@ -1,31 +1,44 @@
 (function ($) {
-  $.fn.load = function () {
+  $.fn.load = function (action = "load") {
     return this.each(function () {
-      let divToAppend = document.createElement("div");
+      switch (action) {
+        case "load":
+          let divToAppend = document.createElement("div");
 
-      divToAppend.id = "loader";
+          divToAppend.id = "loader";
 
-      $(divToAppend).css({
-        width: "100%",
-        height: "100%",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        "text-align": "center",
-        "background-color": "rgba(0, 0, 0, 0.4)",
-      });
+          $(divToAppend).css({
+            width: "100%",
+            height: "100%",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            "text-align": "center",
+            "background-color": "rgba(0, 0, 0, 0.4)",
+          });
 
-      let divLoad = document.createElement("div");
-      divLoad.className = "lds-dual-ring";
-      $(divLoad).css({
-        position: "absolute",
-        top: "50%",
-        left: "50",
-      });
+          let divLoad = document.createElement("div");
+          divLoad.className = "lds-dual-ring";
+          $(divLoad).css({
+            position: "absolute",
+            top: "50%",
+            left: "50",
+          });
 
-      $(divToAppend).append(divLoad);
+          $(divToAppend).append(divLoad);
 
-      $(this).append(divToAppend);
+          $(this).append(divToAppend);
+          break;
+
+        case "unload":
+          $("#loader").fadeOut(function () {
+            $(this).remove();
+          });
+          break;
+
+        default:
+          console.error("Informe uma ação válida");
+      }
     });
   };
 })(jQuery);
